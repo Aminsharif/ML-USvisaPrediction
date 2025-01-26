8f0d5a934a554759c841228e26dcd232ce330a76
Amin Sharif <33375704+Aminsharif@users.noreply.github.com>
2025-01-26 18:25:51 +0600

    added logging, exception and main.util

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="us_visa",
+    version="0.0.0",
+    author="Amin Sharif",
+    author_email="sharifmia236@gmail.com",
+    packages=find_packages()
+)
